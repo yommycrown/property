754454fb7008519e9987bdc3cac5df2052f88d31
Abayomi Adetona <96451683+yommycrown@users.noreply.github.com>
2025-03-26 02:27:24 +0100

    new edit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+
+// DATE SCRIPT
+  let d = new Date();
+        let year = d.getFullYear();
+        document.querySelector(".productionDate").innerHTML = year;
+
+
+
+
+        // Get the button
+        let mybutton = document.getElementById("myBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () { scrollFunction() };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+
+
+        function myFunction() {
+            document.querySelector('.serviceMore').style.display = "block";
+        }
+
